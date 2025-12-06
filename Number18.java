4c5c18704dcd90f8b95e5e58bfc8b4c5a53bed3b
Glydel Mary Rose Visto <glydelmaryrose@gmail.com>
2025-12-07 04:01:11 +0800

    Update Number18.java

--- Number18.java
+++ Number18.java
@@ -10,14 +10,19 @@ public class Number18
 {
     public static void main(String[] args) {
           Scanner scanner = new Scanner(System.in);
+        
           System.out.print("Enter the size of the array: ");
+        
           int size = scanner.nextInt();
           int[] array = new int[size];
+        
           System.out.println("Enter the elements:");
+        
           for (int i = 0; i < size; i++) {
               array[i] = scanner.nextInt();
           }
           boolean hasDup = false;
+        
           for (int i = 0; i < array.length; i++) {
               for (int j = i + 1; j < array.length; j++) {
                   if (array[i] == array[j]) {
